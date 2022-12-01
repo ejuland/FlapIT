ae1a1bdfa0184e28d09f70f23684b09dc6325352
Eric Julander <ejuland@student.wgu.edu>
2022-12-01 08:39:41 -0600

    Added mobile support

--- game.js
+++ game.js
@@ -5,22 +5,22 @@ import { Bounds, Block, Pipe, Character } from "./GameObject.js";
 const TARGET_FPS = 60;
 
 const LEVEL_SETTINGS = {
-    Normal:{
+    Normal: {
         spacing: 4,
         gap: 3,
-        threshold:10
+        threshold: 10
     },
-    Difficult:{
+    Difficult: {
         spacing: 3,
         gap: 3,
         threshold: 3
     },
-    Hard:{
+    Hard: {
         spacing: 2,
         gap: 4,
         threshold: 30
     },
-    Extreme:{
+    Extreme: {
         spacing: 2,
         gap: 3,
     }
@@ -42,12 +42,14 @@ export default class Game {
             document.body.clientWidth;
 
         let newHeight = this.WIDTH
-
+        if (this.WIDTH / window.devicePixelRatio < 500)
+            this.isMobile = true;
+        console.log(this.isMobile);
         this.SCREEN.setAttribute("width", window.innerWidth);
         this.SCREEN.setAttribute("height", window.innerHeight);
 
     }
-
+    isMobile = false;
     GameLoopInterval = null;
     pipes = [];
     renderer = null;
@@ -63,8 +65,8 @@ export default class Game {
         this.pipes.forEach(pipe => pipe.update(this.ScreenBounds, this.character, this.audioPlayer));
         let removed = this.removeUsedPipes();
         this.setSpacingAndGap();
-        for(let pipe = 0; pipe < removed; pipe++)
-            this.createPipe(this.pipes[this.pipes.length-1].Bounds.X+this.blockSize*this.blockSpacing)
+        for (let pipe = 0; pipe < removed; pipe++)
+            this.createPipe(this.pipes[this.pipes.length - 1].Bounds.X + this.blockSize * this.blockSpacing)
     }
 
     render() {
@@ -73,22 +75,25 @@ export default class Game {
         this.pipes.forEach(pipe => pipe.render(this.renderer));
     }
 
-    setSpacingAndGap(){
-        if(this.totalPipesPassed < LEVEL_SETTINGS.Normal.threshold){
+    setSpacingAndGap() {
+        if (this.totalPipesPassed < LEVEL_SETTINGS.Normal.threshold) {
             this.gapSize = LEVEL_SETTINGS.Normal.gap;
             this.blockSpacing = LEVEL_SETTINGS.Normal.spacing;
         }
-        else if(this.totalPipesPassed > LEVEL_SETTINGS.Normal.threshold && this.totalPipesPassed < LEVEL_SETTINGS.Difficult.threshold){
+        else if (this.totalPipesPassed > LEVEL_SETTINGS.Normal.threshold && this.totalPipesPassed < LEVEL_SETTINGS.Difficult.threshold) {
             this.gapSize = LEVEL_SETTINGS.Difficult.gap;
             this.blockSpacing = LEVEL_SETTINGS.Difficult.spacing;
         }
-        else if(this.totalPipesPassed > LEVEL_SETTINGS.Difficult.threshold && this.totalPipesPassed < LEVEL_SETTINGS.Hard.threshold){
+        else if (this.totalPipesPassed > LEVEL_SETTINGS.Difficult.threshold && this.totalPipesPassed < LEVEL_SETTINGS.Hard.threshold) {
             this.gapSize = LEVEL_SETTINGS.Hard.gap;
             this.blockSpacing = LEVEL_SETTINGS.Hard.spacing;
-        }else{
+        } else {
             this.gapSize = LEVEL_SETTINGS.Extreme.gap;
             this.blockSpacing = LEVEL_SETTINGS.Extreme.spacing;
         }
+
+        if(this.isMobile)
+            this.gapSize+=1;
     }
 
     gameOver = true;
@@ -100,25 +105,25 @@ export default class Game {
 
     lastPosition = 3;
 
-    getNewPosition(position){
-        let newPosition = position + Math.floor(Math.random() * 4)-2;
+    getNewPosition(position) {
+        let newPosition = position + Math.floor(Math.random() * 4) - 2;
 
-        if(newPosition == this.lastPosition)
+        if (newPosition == this.lastPosition)
             return this.getNewPosition(position);
-        if(newPosition < 0 || newPosition+this.gapSize > 10)
+        if (newPosition < 0 || newPosition + this.gapSize > 10)
             return this.getNewPosition(position);
-        
+
         return newPosition;
     }
 
-    createPipe(xOffset){
+    createPipe(xOffset) {
         this.lastPosition = this.getNewPosition(this.lastPosition);
-        this.pipes.push(new Pipe(this.lastPosition, 10-this.gapSize, this.ScreenBounds, xOffset));
+        this.pipes.push(new Pipe(this.lastPosition, 10 - this.gapSize, this.ScreenBounds, xOffset));
     }
 
-    removeUsedPipes(){
+    removeUsedPipes() {
         let startingPipes = this.pipes.length;
-        this.pipes = this.pipes.filter(pipe=>!pipe.offScreenLeft);
+        this.pipes = this.pipes.filter(pipe => !pipe.offScreenLeft);
         let missingPipes = startingPipes - this.pipes.length;
         this.totalPipesPassed += missingPipes;
         return missingPipes;
@@ -147,17 +152,19 @@ export default class Game {
 
         this.SCREEN = screen;
         this.CTX = this.SCREEN.getContext("2d");
+        if (this.isMobile)
+            this.CTX.scale(2,2);
         this.renderer = new Renderer(this.CTX);
         this.resize();
         this.ScreenBounds = new Bounds(0, 0, this.WIDTH, this.HEIGHT)
-        this.blockSize =(this.ScreenBounds.Height / 10);
-        this.character = new Character(this.ScreenBounds.Width/2, Math.floor(Math.random() * 300), this.blockSize);
+        this.blockSize = (this.ScreenBounds.Height / 10);
+        this.character = new Character(!this.isMobile ? this.ScreenBounds.Width / 2 : this.blockSize, Math.floor(Math.random() * 300), this.blockSize);
         this.setSpacingAndGap();
-        for(let pipes = 0; pipes < 20; pipes++)
-            this.createPipe((this.ScreenBounds.Width - this.blockSize) +pipes*this.blockSize*this.blockSpacing);
+        for (let pipes = 0; pipes < 20; pipes++)
+            this.createPipe(((this.ScreenBounds.Width - this.blockSize) + this.isMobile ? 2000 : 0) + pipes * this.blockSize * this.blockSpacing);
         this.audioPlayer = new AudioAssetPlayer();
         window.addEventListener("keyup", this.handleInput.bind(this));
         window.addEventListener("mousedown", this.handleInput.bind(this));
-        window.addEventListener("touchend", this.handleInput.bind(this));
+        window.addEventListener("click", this.handleInput.bind(this));
     }
 }
